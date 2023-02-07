42f5ec1a71917c0906b3ae6b4e323c5545e35344
lycia <safinalycia@mail.ru>
2023-02-07 14:00:11 +0300

    добавлены Assert

---
+++ src/main/java/org/example/DZ4/TriangleException.java
@@ -0,0 +1,7 @@
+package org.example.DZ4;
+
+public class TriangleException extends Exception{
+    public TriangleException(String str) {
+        super(str);
+    }
+}
--- src/test/java/org/example/DZ4/TriangleTest.java
+++ src/test/java/org/example/DZ4/TriangleTest.java
@@ -1,24 +1,37 @@
 package org.example.DZ4;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class TriangleTest {
-    public double triangleTest(double a, double b, double c) throws FunctionTriangle{
-
-        if (checkValues(a, b, c));
-        double p = (a + b + c)/2;
-        double s = Math.sqrt(p * (p - a)* (p - b)* (p - c));
-        return s;
-
-
+    public static Logger logger = LoggerFactory.getLogger(TriangleTest.class);
+    @Test
+    void testCalcTriangleArea() throws TriangleException {
+        Triangle triangle = new Triangle();
+        Assertions.assertEquals(14.696938456699069, triangle.triangle(5, 6, 7));
+        logger.info("Позитивный тест: Сравнение фактического результата с ожидаемым результатом.");
     }
 
-    private static boolean checkValues(double a, double b, double c) throws FunctionTriangle {
-        if (a<=0 || b<=0 || c<=0){
-            throw new FunctionTriangle("Одно из значений треугольника неверно");
-        }
-        if (a + b <=c || a + c <=b || b + c <=a){
-            throw new FunctionTriangle("Треугольника не существует");
-        }
+    @Test
+    void testNegativeSideValue() throws TriangleException{
+        Triangle triangle = new Triangle();
+        Assertions.assertThrows(TriangleException.class, ()-> triangle.triangle(5, 6, -7));
+        logger.info("Негатиный тест: Проверка при введении отрицательного значения одной из сторон.");
+    }
 
-        return false;
+    @Test
+    void testZeroSideValue()  throws TriangleException{
+        Triangle triangle = new Triangle();
+        Assertions.assertThrows(TriangleException.class, ()-> triangle.triangle(5, 0, 7));
+        logger.info("Негатиный тест: Проверка при введении нулевого значения одной из сторон.");
+    }
+
+    @Test
+    void testSumTwoSides() throws TriangleException {
+        Triangle triangle = new Triangle();
+        Assertions.assertThrows(TriangleException.class, ()-> triangle.triangle(12, 6, 7));
+        logger.info("Негатиный тест: Проверка при введении значения одной из сторон, которое больше либо равно сумме двух других сторон.");
     }
 }
